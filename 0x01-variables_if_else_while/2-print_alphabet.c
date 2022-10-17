9f057b46bd97c914ad14a2ced41652346ad9536d
ayeniOlajide <olajidezoe@gmail.com>
2022-10-17 01:00:35 -0700

    Print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,37 +3,20 @@
 #include <time.h>
 
 /**
- * main-progrtam entry point
- * Return:0 success, non zero fail
+ * main - print alpha in lower and upper case
+ * Return: Always 0
  */
 int main(void)
 {
-	int m, i, j;
+	char ch;
 
-	for (i = '0'; i <= '9'; i++)
-	{
-		for (j = '0'; j <= '9'; j++)
-		{
-			for (m = '0'; m <= '9'; m++)
-			{
-				if (i < j && j < m)
-				{
-					putchar(i);
-					putchar(j);
-					putchar(m)
-						if (i == '7' && j == '8' && m == '9')
-						{
-							break;
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
-				}
-			}
-		}
-	}
-	putchar ('\n');
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	putchar(ch);
+
+	putchar('\n');
+
 	return (0);
 }
