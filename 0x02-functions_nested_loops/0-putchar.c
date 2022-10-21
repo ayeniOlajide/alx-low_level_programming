00538db6a54047236da1d5a19d3741a2a8139963
ayeniOlajide <olajidezoe@gmail.com>
2022-10-21 03:02:33 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include "main.h"
+include "main.h"
 
 /**
- * main - Prints Holberton as a message.
- *
+ * main - Prints _putchar and a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
