24f65a2c13ca16c4f8939d1fada888395b19ffc9
ayeniOlajide <olajidezoe@gmail.com>
2022-10-23 08:44:56 -0700

    ok

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -10,21 +10,3 @@ void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN.H */
 
--create a 0-main.c file with the content below..
-
-#include "main.h"
-
-/**
- *main - tests function that prints if integer is positive or negative
- *Return: 0
- */
-
-int main(void)
-{
-	int i;
-
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
-}
