d8551e3e0b16ad181279314dd2a047669037afa4
ayeniOlajide <olajidezoe@gmail.com>
2022-10-27 12:25:09 -0700

    ok

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 /**
  * cap_string - capitalizes all words of a string
  * @s: input string.
@@ -19,7 +19,7 @@ char *cap_string(char *s)
 		{
 			if (*(s + count) == sep_words[i])
 			{
-				if (((s + (count + 1)) >= 97) && ((s + (count + 1)) <= 122))
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
 					*(s + (count + 1)) = *(s + (count + 1)) - 32;
 				break;
 			}
